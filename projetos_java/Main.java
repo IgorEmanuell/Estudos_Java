f3b49f1e293e332373458c049c64387497085622
Igor Emanuell <emanuelligor@hotmail.com>
2024-06-04 00:18:30 -0300

    first commit

---
+++ projetos_java/Main.java
@@ -0,0 +1,6 @@
+package projetos_java;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ teste_processamento.java
@@ -0,0 +1,68 @@
+public class teste_processamento {
+    public static void main(String[] args) {
+        // Exemplo de inteiro e double
+        // int x;
+        // double y;
+        
+        // x = 5;
+        // y = 2;
+        // y = 2 * x;
+
+        // System.out.println(x);
+        // System.out.println(y);
+        // double b, B, h, area;
+        // b = 6.0;
+        // B = 8.0;
+        // h = 5.0;
+
+        // area = (b + B) / 2.0 * h;
+        // System.out.println(area);
+
+        // Exemplo de float
+        // float b, B, h, area;
+        // b = 6f;
+        // B = 8f;
+        // h = 5f;
+
+        // area = (b + B) / 2f * h;
+        // System.out.println(area);
+
+        // Exemplo de double
+        // double b, B, h, area;
+        // b = 6.0;
+        // B = 8.0;
+        // h = 5.0;
+
+        // area = (b + B) / 2.0 * h;
+        // System.out.println(area);
+        
+        // Exemplo de int
+        // int a, b;
+        // double resultado;
+        // a = 5;
+        // b = 2;
+        //Para que o resultado saia como double e não em valor inteiro, devemos adiconar o (double) no resultado caso a entrada seja int
+        // resultado = (double) a / b;
+        // System.out.println(resultado);
+
+        // Modificando type do dado
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a;
+
+        System.out.println(b);
+    }
+}
+
+
+    // public static void main(String[] args) {
+    //     double b, B, h, area;
+    //     b = 6.0;
+    //     B = 8.0;
+    //     h = 5.0;
+
+    //     area = (b + B) / 2.0 * h;
+    //     System.out.println(area);
+    // }
